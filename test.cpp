d82a7c0cb06147f362e78a4a7196eeccddd24a32
SamGaaWaa <100835324+SamGaaWaa@users.noreply.github.com>
2022-11-18 20:48:32 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,11 +15,11 @@ void c_str_test() {
 
 void file_test() {
     try {
-        const char *path = R"(face.json)";
+        const char *path = R"(test/face.json)";
         auto obj = json::parse(path, 1);        //缓冲区设置为1，逐个字符解析
         std::cout << "size=" << obj.size() << '\n';
         std::cout << "json:\n" << json::to_json(obj) << '\n';
-        std::ofstream os{R"(result.json)"};
+        std::ofstream os{R"(test/result.json)"};
 
         obj.insert({"array", json::array{{ {123}, {"123"} }}});
 
